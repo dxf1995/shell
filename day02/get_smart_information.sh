a86dc7d992514179b6f441c13123871c5dd00b66
dxf1995 <861272006@qq.com>
2021-01-26 20:14:54 +0800

    第三次提交代码 时间:2021年1月26日 夜间阴

--- day02/get_smart_information.sh
+++ day02/get_smart_information.sh
@@ -6,18 +6,22 @@
 #FileName： get_smart_infomation.sh
 #获取当前所有硬盘smart信息 并存于当前目录之下
 #********************************************************************
+
+# 当前
 disk_number=`lsscsi |awk '{print NR}' |tail -n 1`
+
 i=1 
+
+file_aisle=`pwd`
+
+# 遍历当前所有硬盘且抓取每个硬盘的smart信息
+
 while (( i<=disk_number )); do
 	
-	disk_LC=`lsscsi |awk -F '/' '{print $3}'|sed -n "$total p"`
-	
-	disk_info=`smartctl --all /dev/$disk_LC` >>./ kill.txt
-	echo disk_info
-	
+	disk_LC=`lsscsi |awk -F '/' '{print $3}'|sed -n "$i p"`
+	echo "$disk_LC 硬盘"
+    smartctl --all /dev/$disk_LC >> $file_aisle/kill.log
+	echo $disk_info
+	(( i++ ))
 done
-
-#disk_number=`lsscsi |awk '{print NR}' |tail -n 1`
-
-#disk_LC=`lsscsi |awk -F '/' '{print $3}'|sed -n "$total p"`
 
